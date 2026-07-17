069432122b4043d5f3f1e0ce0b4f02ae8dfe7833
Sergio Córdoba <sergiocordoba246@gmail.com>
2026-07-17 18:57:07 +0200

    Mejora base de datos

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/authController');
 const { loginLimiter } = require('../middleware/rateLimiter');
 
 router.post('/login', loginLimiter, authController.login);
-router.post('/create', authController.createAdmin);
+// router.post('/create', authController.createAdmin);
 
 module.exports = router;
